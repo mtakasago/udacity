44823029f65d54c109d63f2687ec8cf9eebae5cc
mtakasago <takasago_marika@global.kawasaki.com>
2024-07-23 16:34:41 +0900

    update blur_factor_improved.cpp

--- optimize/Static/blur_factor_improved.cpp
+++ optimize/Static/blur_factor_improved.cpp
@@ -3,16 +3,15 @@
 using namespace std;
 
 vector < vector <float> > blur_factor_improved() {
-    
+
     // calculate blur factors
     static float blurring = .12;
     static float center = 1.0 - blurring;
     static float corner = blurring / 12.0;
     static float adjacent = blurring / 6.0;
-    
+
     int i, j;
-    float val;
-    
+
     // create the blur window
     static vector< vector<float> > window = {{corner, adjacent, corner}, {adjacent, center, adjacent}, {corner, adjacent, corner}};
     return window;
